0de8e8a30a9235b746346c44f250a7880141d1c6
Подорин Александр <alex@podorin.ru>
2019-06-07 18:28:51 +0300

    Updates

---
+++ precache-manifest.bef953b4ebd4eaa2cfaa85f3ac3a0b8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3118d42a438abdf52eb2",
+    "url": "/vkapps_notifier/static/js/runtime~main.3118d42a.js"
+  },
+  {
+    "revision": "f6ab2519d6fb690cf562",
+    "url": "/vkapps_notifier/static/js/main.f6ab2519.chunk.js"
+  },
+  {
+    "revision": "6bb017a658bb39d4fd7a",
+    "url": "/vkapps_notifier/static/js/2.6bb017a6.chunk.js"
+  },
+  {
+    "revision": "6bb017a658bb39d4fd7a",
+    "url": "/vkapps_notifier/static/css/2.5b66982b.chunk.css"
+  },
+  {
+    "revision": "fa0f1a915924e6f2e73f3662719c032d",
+    "url": "/vkapps_notifier/index.html"
+  }
+];
