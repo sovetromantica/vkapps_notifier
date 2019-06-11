afcc247176f641908e9c2157bab246cc3b8b28d5
Подорин Александр <alex@podorin.ru>
2019-06-11 11:22:19 +0300

    Updates

--- precache-manifest.2a625d56e8d755abeaa0087fc177cddd.js
+++ precache-manifest.2a625d56e8d755abeaa0087fc177cddd.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/vkapps_notifier/static/js/runtime~main.3118d42a.js"
   },
   {
-    "revision": "a0dc631508da4b72b0e7",
-    "url": "/vkapps_notifier/static/js/main.a0dc6315.chunk.js"
+    "revision": "0aa9162e1787a061fcd8",
+    "url": "/vkapps_notifier/static/js/main.0aa9162e.chunk.js"
   },
   {
     "revision": "9e5c3ed966b95a75f843",
     "url": "/vkapps_notifier/static/js/2.9e5c3ed9.chunk.js"
   },
   {
-    "revision": "a0dc631508da4b72b0e7",
+    "revision": "0aa9162e1787a061fcd8",
     "url": "/vkapps_notifier/static/css/main.a30bae8b.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/vkapps_notifier/static/css/2.9283af8e.chunk.css"
   },
   {
-    "revision": "faa66f0629605224ddfa430eaea92812",
+    "revision": "f06ea50c98f0b9b5431d5fa2a99020c1",
     "url": "/vkapps_notifier/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vkapps_notifier/precache-manifest.1c815ac0844f757b834dfc49dafea34e.js"
+  "/vkapps_notifier/precache-manifest.2a625d56e8d755abeaa0087fc177cddd.js"
 );
 
 workbox.clientsClaim();
