6d45ae56e73e6e65abd2fd541057faeeeb0d3855
Подорин Александр <alex@podorin.ru>
2020-08-14 19:11:12 +0300

    Updates

---
+++ precache-manifest.c5ead6362f96b9fb4da3cb294a89b43c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b26d02987a3260d38c865ff2f9375b",
+    "url": "/vkapps_notifier/index.html"
+  },
+  {
+    "revision": "4682b1711563932d5eec",
+    "url": "/vkapps_notifier/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "91f6a0938b841f744665",
+    "url": "/vkapps_notifier/static/css/main.962ea9ec.chunk.css"
+  },
+  {
+    "revision": "4682b1711563932d5eec",
+    "url": "/vkapps_notifier/static/js/2.31bfa642.chunk.js"
+  },
+  {
+    "revision": "4f44a17bb08e78ed1b92485defc539ac",
+    "url": "/vkapps_notifier/static/js/2.31bfa642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f6a0938b841f744665",
+    "url": "/vkapps_notifier/static/js/main.e964c3b1.chunk.js"
+  },
+  {
+    "revision": "0f40df510dfb18cc65a8",
+    "url": "/vkapps_notifier/static/js/runtime-main.34cf26b0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vkapps_notifier/precache-manifest.f32a1161383466ed347d199eb8e0dbde.js"
+  "/vkapps_notifier/precache-manifest.c5ead6362f96b9fb4da3cb294a89b43c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vkapps_notifier/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vkapps_notifier/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
